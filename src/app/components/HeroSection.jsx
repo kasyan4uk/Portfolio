e593b4857ee58adc0bdfc0a3dc0d75bd40675154
Serhii Kasianchuk <k.kasan2016@gmail.com>
2024-07-26 22:32:37 +0300

    section about finish

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -20,8 +20,6 @@ const HeroSection = () => {
                 1000, 
                 "Web Developer",
                 1000,
-                "Mobile Developer",
-                1000,
               ]}
               wrapper="span"
               speed={50}
